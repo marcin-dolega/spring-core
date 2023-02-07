359d7c524deb9f47ef99baa258cc059c19d59ee5
Marcin <marcin92d@gmail.com>
2023-02-07 15:52:03 +0100

    Initial commit

---
+++ src/main/java/pl/dolega/springcore/SpringcoreApplication.java
@@ -0,0 +1,13 @@
+package pl.dolega.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dolega/springcore/SpringcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dolega.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
